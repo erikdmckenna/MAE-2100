54e634f46c589275e99c23e453f5235fcf8e66c4
erikdmckenna <erikdmckenna@gmail.com>
2022-09-22 10:47:53 -0400

    Initial commit

---
+++ Thermo3_17.py
@@ -0,0 +1,20 @@
+"""
+@author: Harsha Chelliah
+"""
+
+# Estimating Error of Ideal Gas EoS
+
+from CoolProp import CoolProp as CP
+import CoolProp.Plots as CPP
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+# to print out all the fluid substances available in CoolProp database:
+print(CP.FluidsList())
+
+fluid = 'Ammonia'
+
+T1 = 293.15 # K
+D1 = CP.PropsSI('D', )
+T2 = 313.15 # K
